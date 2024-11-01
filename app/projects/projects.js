86d78df0eecea48eca1c056273d08a6fe133367c
Ernesto Ibanez <ernestoibanez@Ernestos-MBP-2.home>
2024-11-01 13:30:01 -0700

    took off website for keyword extractor project

--- app/projects/projects.js
+++ app/projects/projects.js
@@ -14,7 +14,6 @@ export const projects = [
          description: "Keyword Extractor is a Python-based tool that processes textual data to extract the most relevant keywords using the TF-IDF algorithm. This tool is designed to assist researchers, SEO specialists, and job searchers in identifying key terms that define the textual content of documents.",
          tech: "Python, Flask, NLTK, Scikit-learn, HTML/CSS, Heroku",
          imgUrl: "/project_pics/keyword_extractor_img.png",
-         website: "https://keyword-extractor-ad3cc04515a2.herokuapp.com/",
          git: "https://github.com/ernestoIJ/job-description-keyword-extractor",
          date: "2024-07-31",
       },
