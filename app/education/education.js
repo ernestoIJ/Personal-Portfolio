ae8d5bf42fb29135e948d528ab5a8eecc5a83c55
Ernesto Ibanez <ernestoibanez@Ernestos-MBP-2.home>
2024-11-02 10:42:50 -0700

    fixing typos

--- app/education/education.js
+++ app/education/education.js
@@ -2,7 +2,7 @@ export const educations = [
     {
         id: 1,
         school: "Glendale Community College",
-        degree: "Associates",
+        degree: "Associate",
         schoolLogo: "/school_logos/Glendale_Community_College_logo.png", 
         startDate: "Aug 2021",
         endDate: "Dec 2023",
@@ -10,7 +10,7 @@ export const educations = [
     {
         id: 2,
         school: "Arizona State University",
-        degree: "Bachelors of Science",
+        degree: "Bachelor of Science",
         schoolLogo: "/school_logos/ASU_logo.png", 
         startDate: "Jan 2024",
         endDate: "Dec 2025",
